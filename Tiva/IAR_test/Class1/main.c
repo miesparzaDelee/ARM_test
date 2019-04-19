320938e73816d03884524d52fedc994ba455efbf
Miguel Esparza <42750822+miesparzaDelee@users.noreply.github.com>
2019-04-19 14:18:04 -0500

    update program with gpio diret addressing

--- Tiva/IAR_test/Class1/main.c
+++ Tiva/IAR_test/Class1/main.c
@@ -1,16 +1,21 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
 
+#define LED_RED (1U<<1)
+#define LED_BLUE (1U<<2)
+#define LED_GREEN (1U<<3)
 
 int main()
 {
-  SYSCTL_RCGCGPIO_R = 0x20U;
-  GPIO_PORTF_DIR_R = 0x0EU;
-  GPIO_PORTF_DEN_R = 0x0EU;
+   
+  SYSCTL_RCGCGPIO_R |= (1U<<5);
+  GPIO_PORTF_DIR_R |= (LED_RED | LED_BLUE | LED_GREEN);
+  GPIO_PORTF_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);
   
+  GPIO_PORTF_DATA_BITS_R[LED_BLUE]  |= 0xFFFFFFFF;
   while(1){
- GPIO_PORTF_DATA_R  = 0x02U;
- GPIO_PORTF_DATA_R  = 0x00U;
+ GPIO_PORTF_DATA_BITS_R[2] = 0xFFFFFFFF;
+ GPIO_PORTF_DATA_BITS_R[2] = 0;
   }
   return 0;
 }
