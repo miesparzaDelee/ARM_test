ac9b6280c1e9a498af5d779112aa8d1f35a149f0
Miguel Esparza <42750822+miesparzaDelee@users.noreply.github.com>
2019-04-23 10:02:36 -0500

    update

--- Tiva/IAR_test/Class1/main.c
+++ Tiva/IAR_test/Class1/main.c
@@ -5,6 +5,15 @@
 #define LED_BLUE (1U<<2)
 #define LED_GREEN (1U<<3)
 
+void delay(int value);
+
+void delay(int value){
+int volatile counter=0;
+while (counter<value){
+  counter++;
+}
+}  
+
 int main()
 {
    
@@ -13,9 +22,13 @@ int main()
   GPIO_PORTF_DEN_R |= (LED_RED | LED_BLUE | LED_GREEN);
   
   GPIO_PORTF_DATA_BITS_R[LED_BLUE]  |= 0xFFFFFFFF;
+  
+  
   while(1){
- GPIO_PORTF_DATA_BITS_R[2] = 0xFFFFFFFF;
+ GPIO_PORTF_DATA_BITS_R[2] = 0xF;
+ delay(100000);
  GPIO_PORTF_DATA_BITS_R[2] = 0;
+ delay(100000);
   }
   return 0;
 }
